b399e39c8931aea9ac5491f3e290f9537bbb05d1
Csondes Maja <e4ec88@hallgato.nje.hu>
2025-11-22 14:26:03 +0100

    Kapcsolat fül

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,28 +5,45 @@
 
     <h1>Kapcsolat</h1>
 
-    @if(session('success'))
-        <div style="color: green; margin-bottom: 10px;">
-            {{ session('success') }}
-        </div>
-    @endif
+    @auth
+        @if(session('success'))
+            <div style="color: green; margin-bottom: 10px;">
+                {{ session('success') }}
+            </div>
+        @endif
 
-    <p>
-        Bejelentkezve mint: <b>{{ auth()->user()->name }}</b> ({{ auth()->user()->email }})
-    </p>
+        <p>
+            Bejelentkezve mint: <b>{{ auth()->user()->name }}</b> ({{ auth()->user()->email }})
+        </p>
 
-    <form action="{{ route('contact.store') }}" method="POST">
-        @csrf
+        <form action="{{ route('contact.store') }}" method="POST">
+            @csrf
 
-        <label>Üzenet:</label><br>
-        <textarea name="message" rows="5" style="width: 100%;">{{ old('message') }}</textarea>
-        @error('message')
-            <div style="color:red;">{{ $message }}</div>
-        @enderror
+            <label>Üzenet:</label><br>
+            <textarea name="message" rows="5" style="width: 100%;">{{ old('message') }}</textarea>
+            @error('message')
+                <div style="color:red;">{{ $message }}</div>
+            @enderror
 
-        <br><br>
-        <button type="submit">Üzenet küldése</button>
-    </form>
+            <br><br>
+            <button type="submit">Üzenet küldése</button>
+        </form>
+    @else
+        <p>Üzenet küldéséhez jelentkezzen be!</p>
+        <form action="/profil" method="POST">
+            @csrf
+            <input type="hidden" name="redirect_to" value="{{ url()->current() }}">
+            <input name='email' type="text" placeholder="E-mail cím" required><br>
+            <input name='password' type="password" placeholder="Jelszó" required><br>
+            @if ($errors->has('email'))
+                <p style="color: red;">{{ $errors->first('email') }}</p>
+            @endif
+            @if ($errors->has('password'))
+                <p style="color: red;">{{ $errors->first('password') }}</p>
+            @endif
+            <button type="submit" name="action" value="login">Bejelentkezés</button>
+        </form>
+    @endauth
 
 </div>
 @endsection
